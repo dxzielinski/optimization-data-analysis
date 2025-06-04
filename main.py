0ef36ad7dd2a5def1baa907acc6bacac6faa6b63
dxzielinski <dominik.zielinski2002@gmail.com>
2025-06-05 00:56:57 +0200

    add both trained models for eurosat with plots

---
+++ main.py
@@ -0,0 +1,48 @@
+"""
+Example main script to test a trained WideResNet model on the EuroSAT dataset.
+"""
+
+import torch
+import torchvision
+from lit_modules.custom_wide_resnet_lit import WideResnetLit
+import lightning as L
+from torchvision import transforms
+
+checkpoint_path = (
+    "model_training_notebooks/checkpoints/wide_resnet/epoch=49-val_acc=0.944.ckpt"
+)
+with open("test_indices.txt", "r") as f:
+    test_indices = [int(line.strip()) for line in f.readlines()]
+
+
+def main():
+    model = WideResnetLit.load_from_checkpoint(checkpoint_path)
+    trainer = L.Trainer(
+        logger=None,
+        enable_progress_bar=True,
+    )
+    IMAGE_SIZE = 32
+    mean, std = [0.485, 0.456, 0.406], [0.229, 0.224, 0.225]
+    transform_compose = transforms.Compose(
+        [
+            transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean, std),
+        ]
+    )
+    dataset = torchvision.datasets.EuroSAT(
+        root="model_training_notebooks/data",
+        download=False,
+        transform=transform_compose,
+    )
+    test_set = torch.utils.data.Subset(
+        dataset,
+        test_indices,
+    )
+    test_loader = torch.utils.data.DataLoader(
+        test_set,
+        batch_size=64,
+        shuffle=False,
+        num_workers=4,
+    )
+    trainer.test(model, datamodule=test_loader)
